fce1b3d0abd677e73ff455a16fa0161ac55b1509
shinkarenkov <112132667+shinkarenkov@users.noreply.github.com>
2023-06-21 01:45:10 +0300

    2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+ingredients.map((ingredient) => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredient;
+  console.log(itemEl)
+  const ulEl = document.querySelector('#ingredients')
+ulEl.appendChild(itemEl)
+console.log(ulEl)
+}).join(``);
+
+
+
